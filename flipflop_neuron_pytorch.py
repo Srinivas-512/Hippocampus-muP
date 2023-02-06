cd5c05a7cce28dd6e7f54e71ec2949335f7ce32e
Srinivas-512 <97453967+Srinivas-512@users.noreply.github.com>
2023-02-06 23:28:17 +0530

    Flip flop fixed

--- flipflop_neuron_pytorch.py
+++ flipflop_neuron_pytorch.py
@@ -8,9 +8,9 @@ class FlipFlop(nn.Module):
         self.units = units
         self.state_size = units
         self.input_size = input_size
-        self.j_h = nn.Linear(self.input_size, self.units) # jk of flip flop
+        self.j_h = nn.Linear(self.units, self.units) # jk of flip flop
         self.j_x = nn.Linear(self.input_size, self.units) # basicaaly models lstm with some gru features
-        self.k_h = nn.Linear(self.input_size, self.units) # units is no of neurons basically
+        self.k_h = nn.Linear(self.units, self.units) # units is no of neurons basically
         self.k_x = nn.Linear(self.input_size, self.units) # units --> hyperparameter
         self.activation = nn.Sigmoid()
 
@@ -20,3 +20,8 @@ class FlipFlop(nn.Module):
         k = self.activation(self.k_x(inputs) + self.k_h(prev_output))
         output = j * (1 - prev_output) + (1 - k) * prev_output
         return output, [output]
+
+# obj = FlipFlop(5,10)
+# t = torch.rand((1,5))
+# s = torch.rand((1,10))
+# print(obj(t, s))
