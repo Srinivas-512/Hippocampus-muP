301b5e4dc177ccd138d47fed901698faf9fdb96f
Srinivas-512 <97453967+Srinivas-512@users.noreply.github.com>
2023-04-02 18:33:16 +0530

    Encoder

--- Encoder.py
+++ Encoder.py
@@ -10,22 +10,21 @@ class Encoder(nn.Module):
         self.rnn = nn.LSTM(embed_dim, hidden_size, batch_first=True, bidirectional=True)
         self.bidirectional = True
 
-    def forward(self, x, hidden, cell):
-        out = self.embedding(x)#.unsqueeze(1)
-        out, (hidden, cell) = self.rnn(out, (hidden, cell))
+    def forward(self, x):
+        #out = self.embedding(x)#.unsqueeze(1)
+        out = x
+        print(out.shape)
+        out, (hidden, cell) = self.rnn(out)
         out = self.rnn(x)
         return out, hidden, cell
     
-    def init_hidden(self, batch_size):
-        hidden = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
-        cell = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
-        return hidden, cell
+    # def init_hidden(self, batch_size):
+    #     hidden = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
+    #     cell = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
+    #     return hidden, cell
 
-print(int(True))
-obj = Encoder(128, 512, 2)
-input = torch.randint(2, (10, 7))
+obj = Encoder(66, 512, 2)
+input = torch.randint(2, (16, 17, 7))
 print(input)
-hidden, cell = obj.init_hidden(1)
-print(hidden.shape)
-out, _, _ = obj(input, hidden, cell)
+out, _, _ = obj(input)
 print(out.shape)
