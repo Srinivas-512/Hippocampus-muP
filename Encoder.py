f5f453688f155e585efb6f428545aa057e47322a
Srinivasan Kidambi <97453967+Srinivas-512@users.noreply.github.com>
2023-04-02 17:33:36 +0530

    Encoder

--- Encoder.py
+++ Encoder.py
@@ -3,19 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(self, Encoder).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.layer = nn.Sequential(
-            nn.Conv1d(self.in_channels, 32, kernel_size=1, stride=1),
-            nn.MaxPool1d(kernel_size=1, stride=1),
-            nn.Conv1d(32, 64, kernel_size=1, stride=1),
-            nn.MaxPool1d(kernel_size=1, stride=1),
-            nn.BatchNorm1d(num_features = 64),
-            nn.Softmax()
-        )
+    def __init__(self, vocab_size, embed_dim, hidden_size):
+        super(Encoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.hidden_size = hidden_size
+        self.rnn = nn.LSTM(embed_dim, hidden_size, batch_first=True, bidirectional=True)
+        self.bidirectional = True
+
+    def forward(self, x, hidden, cell):
+        out = self.embedding(x)#.unsqueeze(1)
+        out, (hidden, cell) = self.rnn(out, (hidden, cell))
+        out = self.rnn(x)
+        return out, hidden, cell
     
-    def forward(self, x):
-        x = self.layer(x)
-        return x 
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
+        cell = torch.zeros(1+int(self.bidirectional), batch_size, self.hidden_size)
+        return hidden, cell
+
+print(int(True))
+obj = Encoder(128, 512, 2)
+input = torch.randint(2, (10, 7))
+print(input)
+hidden, cell = obj.init_hidden(1)
+print(hidden.shape)
+out, _, _ = obj(input, hidden, cell)
+print(out.shape)
